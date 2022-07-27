48c468a09e406bd71f60cea9320f4582702dca66
秦朝勇 <qinchy@msn.com>
2022-07-27 17:21:59 +0800

    qincy init

--- pkg/plugins/plugins.go
+++ pkg/plugins/plugins.go
@@ -37,17 +37,17 @@ func (s *QinchyScheduler) Name() string {
 
 //PreFilter 预过滤
 func (s *QinchyScheduler) PreFilter(ctx context.Context, state *framework.CycleState, pod *core.Pod) *framework.Status {
-	klog.Infof("PreFilter pod:%#v\n", pod)
+	klog.V(3).Infof("PreFilter pod:%#v\n", pod)
 	return framework.NewStatus(framework.Success, "预过滤通过")
 }
 
 func (s *QinchyScheduler) AddPod(ctx context.Context, state *framework.CycleState, podToSchedule *core.Pod, podToAdd *core.Pod, nodeInfo *nodeinfo.NodeInfo) *framework.Status {
-	klog.Infof("PreFilterExtensions -> AddPod pod:%#v, SchedulePod pod:%#v, nodeInfo:%#v\n", podToAdd, podToSchedule, nodeInfo)
+	klog.V(3).Infof("PreFilterExtensions -> AddPod pod:%#v, SchedulePod pod:%#v, nodeInfo:%#v\n", podToAdd, podToSchedule, nodeInfo)
 	return framework.NewStatus(framework.Success, "添加pod成功")
 }
 
 func (s *QinchyScheduler) RemovePod(ctx context.Context, state *framework.CycleState, podToSchedule *core.Pod, podToRemove *core.Pod, nodeInfo *nodeinfo.NodeInfo) *framework.Status {
-	klog.Infof("PreFilterExtensions -> RemovePod pod:%#v, SchedulePod pod:%#v, nodeInfo:%#v\n", podToRemove, podToSchedule, nodeInfo)
+	klog.V(3).Infof("PreFilterExtensions -> RemovePod pod:%#v, SchedulePod pod:%#v, nodeInfo:%#v\n", podToRemove, podToSchedule, nodeInfo)
 	return framework.NewStatus(framework.Success, "移除pod成功")
 }
 
@@ -58,19 +58,19 @@ func (s *QinchyScheduler) PreFilterExtensions() framework.PreFilterExtensions {
 
 //Filter 过滤节点
 func (s *QinchyScheduler) Filter(ctx context.Context, state *framework.CycleState, pod *core.Pod, nodeInfo *nodeinfo.NodeInfo) *framework.Status {
-	klog.Infof("Filter pod:%#v\n", pod)
+	klog.V(3).Infof("Filter pod:%#v\n", pod)
 	return framework.NewStatus(framework.Success, "预过滤通过")
 }
 
 //Score 打分
 func (s *QinchyScheduler) Score(ctx context.Context, state *framework.CycleState, pod *core.Pod, nodeName string) (int64, *framework.Status) {
-	klog.Infof("Score pod:%s nodeName:%s\n", pod.Name, nodeName)
+	klog.V(3).Infof("Score pod:%s nodeName:%s\n", pod.Name, nodeName)
 	return 10, nil
 }
 
 //NormalizeScore 归一化
 func (s *QinchyScheduler) NormalizeScore(ctx context.Context, state *framework.CycleState, pod *core.Pod, scores framework.NodeScoreList) *framework.Status {
-	klog.Infof("NormalizeScore pod:%v\n", pod.Name)
+	klog.V(3).Infof("NormalizeScore pod:%v\n", pod.Name)
 	return framework.NewStatus(framework.Success, "预过滤通过")
 }
 
@@ -82,17 +82,17 @@ func (s *QinchyScheduler) ScoreExtensions() framework.ScoreExtensions {
 //PreBind 预绑定
 func (s *QinchyScheduler) PreBind(ctx context.Context, state *framework.CycleState, pod *core.Pod, nodeName string) *framework.Status {
 	if nodeInfo, err := s.handle.SnapshotSharedLister().NodeInfos().Get(nodeName); err != nil {
-		klog.Infof("prebind get node info error: %v\n", nodeName)
+		klog.V(3).Infof("prebind get node info error: %v\n", nodeName)
 		return framework.NewStatus(framework.Error, fmt.Sprintf("prebind get node info error: %v", nodeName))
 	} else {
-		klog.Infof("prebind node info: %v\n", nodeInfo.Node().Name)
+		klog.V(3).Infof("prebind node info: %v\n", nodeInfo.Node().Name)
 		return framework.NewStatus(framework.Success, "")
 	}
 }
 
 // New 初始化新的插件
 func New(configuration *runtime.Unknown, f framework.FrameworkHandle) (framework.Plugin, error) {
-	klog.Infof("使用配置：%#v初始化调度器：%s\n",configuration, Name)
+	klog.V(3).Infof("使用配置：%#v初始化调度器：%s\n",configuration, Name)
 	return &QinchyScheduler{
 		handle: f,
 	}, nil
